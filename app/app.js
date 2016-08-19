f6fc902c8af05502a8fd93e57f042c914908fd93
Сергей Нагин Sergey Nagin <Sereja@Sergejs-MacBook-Pro.local>
2016-08-20 00:26:54 +0300

    new commit

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var server = require('http').Server(app);
-server.listen(80);
+server.listen(8080);
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
