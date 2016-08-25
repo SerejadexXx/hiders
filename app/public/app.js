6c57ee467e2a010eef04baeccb3c7bf36ed26a5a
Сергей Нагин Sergey Nagin <Sereja@Sergejs-MacBook-Pro.local>
2016-08-25 16:11:04 +0300

    new commit

--- app/public/app.js
+++ app/public/app.js
@@ -1,5 +1,42 @@
 var module = angular.module('indexApp', []);
 
+module.service('srvAuth', function() {
+    this.watchLoginChange = function() {
+
+        var _self = this;
+
+        FB.Event.subscribe('auth.authResponseChange', function(res) {
+
+            if (res.status === 'connected') {
+
+                /*
+                 The user is already logged,
+                 is possible retrieve his personal info
+                 */
+                _self.getUserInfo();
+
+                /*
+                 This is also the point where you should create a
+                 session for the current user.
+                 For this purpose you can use the data inside the
+                 res.authResponse object.
+                 */
+
+            }
+            else {
+
+                /*
+                 The user is not logged to the app, or into Facebook:
+                 destroy the session on the server.
+                 */
+
+            }
+
+        });
+
+    }
+});
+
 module.run(['$rootScope', '$window', 'srvAuth',
     function($rootScope, $window, sAuth) {
 
@@ -45,7 +82,7 @@ module.run(['$rootScope', '$window', 'srvAuth',
                 xfbml: true
             });
 
-            sAuth.watchAuthenticationStatusChange();
+            sAuth.watchLoginChange();
 
         };
 
